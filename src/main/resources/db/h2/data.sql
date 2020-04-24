142fdb7e0a8cbb8e260aad3245e9ed618775bb65
clnluca <38559351+clnluca@users.noreply.github.com>
2020-04-24 23:24:08 +0300

    frontend

---
+++ src/main/resources/db/h2/data.sql
@@ -0,0 +1,19 @@
+INSERT INTO types VALUES (1, 'bad');
+INSERT INTO types VALUES (2, 'fair');
+INSERT INTO types VALUES (3, 'good');
+INSERT INTO types VALUES (4, 'amazing');
+
+INSERT INTO owners VALUES (1, 'Pushups', 'Pushups', 'push your body', 'Madison', '235');
+INSERT INTO owners VALUES (2, 'Situps', 'steel abs', '', 'Sun Prairie', '123');
+INSERT INTO owners VALUES (3, 'Crunches', 'Crunches', 'work your abs', 'McFarland', '412');
+INSERT INTO owners VALUES (4, 'Plank', 'Plank', 'work your core', 'Windsor', '122');
+INSERT INTO owners VALUES (5, 'Pullups', 'Pullups', 'upper body', 'Madison', '222');
+INSERT INTO owners VALUES (6, 'Bicycle', 'Bicycle', 'good for your legs', 'Monona', '124');
+
+INSERT INTO pets VALUES (1, 'I like this', 1, 1);
+INSERT INTO pets VALUES (2, 'Too hard', 4, 2);
+INSERT INTO pets VALUES (3, 'Hard',  2, 3);
+INSERT INTO pets VALUES (4, '', 2, 3);
+INSERT INTO pets VALUES (5, '',  3, 4);
+INSERT INTO pets VALUES (6, 'Too easy',  4, 5);
+INSERT INTO pets VALUES (7, '',  1, 6);
